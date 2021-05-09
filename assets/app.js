445e91f5c8e172e5fb47de7574830fee20140ac1
Diego Bonilla <drbv27@gmail.com>
2021-05-09 06:57:46 -0500

    Initial Commit: Excercises Approach

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+/*Ejercicios Basicos JS Nivel 1
+1.Programa una función que cuente el número de caracteres de una cadena de texto. Ejemplo: miFuncion("Hola Mundo") devolverá 10.
+2.Programa una función que te devuelva el texto recortado según el número de caracteres indicados. Ejemplo: miFuncion("Hola Mundo", 4) devolverá "Hola".
+3.Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter. Ejemplo: miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+4.Programa una función que repita un texto X veces. Ejemplo: miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
+*/
